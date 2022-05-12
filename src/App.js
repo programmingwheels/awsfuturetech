72f8aaad3264e59edfc239a41390e83f0ba36c78
programmingwheels <leninlawrence111@gmail.com>
2022-05-12 19:28:20 +0530

    Heading has been changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to futuruetech AWS</h1>
+      <h1>Welcome to futuruetech AWS Training</h1>
     </div>
   );
 }
